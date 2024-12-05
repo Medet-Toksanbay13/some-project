0cec501a01c04d9db4e0862829e07a8a1f7089bc
Тоқсанбай Медет Қанатұлы <041113500909@enu.kz>
2024-12-05 11:47:55 +0500

    changes some

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Цифровая платформа для игр</title>
+    <link rel="stylesheet" href="profile.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <h1>LineGames</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="home.html">Главная</a></li>
+                <li><a href="games.html">Игры</a></li>
+                <li><a href="community.html">Сообщество</a></li>
+                <li><a href="profile.html">Профиль</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
+
+
 <?php
 session_start();
 
